8f8f9eed82ed6f4ee0bd5ca8adbae39b2b657656
Guyot <WangGY116411@yimidida.com>
2020-12-29 19:53:39 +0800

    初始化项目

---
+++ src/main/java/com/guyot/practise/Bootstrap.java
@@ -0,0 +1,13 @@
+package com.guyot.practise;
+
+/**
+ * @author guyot
+ * @version 1.0.0-SNAPSHOT
+ * @desc to do list管理系统入口
+ * @date 2020/12/29
+ */
+public class Bootstrap {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
